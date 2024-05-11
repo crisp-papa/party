dbb53bf39a75fb506e40f9f2065e7d62e45c1cb5
floatingeye <eriklougee@gmail.com>
2024-05-11 13:31:11 -0400

    Moving the color choices to the constants.js file - Renaming of constants.json to constants.js - Update driver.js - Added a default color palette for the tiles

---
+++ js/constants.js
@@ -0,0 +1,48 @@
+const constants = {
+	"GAMES": [
+		"Starcraft",
+		"Counter Strike",
+		"Worms",
+		"Diablo II",
+		"Chivalry",
+		"Doom / Doom II",
+		"Prop Hunt",
+		"Terraria Competition",
+		"Mortal Kombat X",
+		"Special Smash Event",
+		"Wheel of Torture",
+		"Heroes of Might & Magic III",
+		"Evan Drink Penalty",
+		"Evan Removes His Pants",
+		"Evan Drink Assignment",
+		"Half Life 2",
+		"Company of Heroes",
+		"Day Z",
+		"Duke3D",
+		"Aliens vs. Predator",
+		"Evan's Choice",
+		"L4D2",
+		"Alien Swarm",
+		"Spin Again",
+	],
+	"MP3": [
+		"o-fortuna.mp3"
+	],
+	"COLORS_DEFAULT": [
+		"246B61",
+		"6BA099",
+		"43857C",
+		"0D5047",
+		"00352E",
+		"2D4671",
+		"051838",
+		"152C55",
+		"4D658D",
+		"7689A9",
+		"408E2F",
+		"0C4700",
+		"216A12",
+		"68B159",
+		"9AD58E"
+	]
+};
